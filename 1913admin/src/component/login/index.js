60f9d5d4ef3a87611086ab692d893e44295120e4
NeverLanderr <1947638416@qq.com>
2019-10-22 21:38:10 +0800

    lijiao2

--- 1913admin/src/component/login/index.js
+++ 1913admin/src/component/login/index.js
@@ -1,5 +1,8 @@
 import React,{Component}from 'react';
-
+import './index.less'
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+// import SidebarLogo from '../admin/SidebarLogo';
+import logo from '../assets/images/logo.png';
 
 class Login extends Component {
 
@@ -8,13 +11,46 @@ class Login extends Component {
     return (
       <div className='login-box'>
         这里是登录页
-        <button onClick={()=>{
-                    this.props.history.push('/admin')
-                }}>Home</button>
+      <div className="sidebar-logo">
+        <img src={logo} alt="..."/>
+        <h2 className="sidebar-logo-content">肥猫超市</h2>
+        
       </div>
-    );
-  }
+      <span className='login-span'>请使用您的账号密码登录系统</span>
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <Form.Item>
+        <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+              />,
+        </Form.Item>
+        <Form.Item>
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />,
+        </Form.Item>
+        <Form.Item>
+        <Checkbox>Remember me</Checkbox>
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </Form.Item>
+      </Form>
+          <button onClick={()=>{
+            this.props.history.push('/admin')
+          }}>Home</button>
+      </div>
+    
+      
+    )
+  
 }
-
+}
 
 export default Login
