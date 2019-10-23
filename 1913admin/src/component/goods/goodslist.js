6e20c55ff9eb8d3391cbd10da333d1128eec144f
jin_1212 <1136517070@qq.com>
2019-10-23 21:57:11 +0800

    wj1023

--- 1913admin/src/component/goods/goodslist.js
+++ 1913admin/src/component/goods/goodslist.js
@@ -1,34 +1,32 @@
 import React,{Component} from 'react';
-import { List, Card,Icon,Input,InputNumber,Button,Pagination} from 'antd';
+import { List, Card,Icon,Input,InputNumber,Button,Pagination,Select} from 'antd';
 import GoodsEdit from './component/goodsedit'
 import WareHousing from './component/warehousing'
 import OutHousing from './component/outhousing'
 import './goodslist.less'
-const data = [
-  {
-    title: '葡萄',
-  },
-  {
-    title: '香蕉',
-  },
-  {
-    title: '桃子',
-  },
-  {
-    title: '梨',
-  },
-];
+const { Option } = Select;
 class GoodsList extends Component {
   constructor(){
     super()
     this.state={
       editState:false,
       comeState:false,
-      outState:false
+      outState:false,
+      editData:null,
+      list:[]
     }
   }
-  changeEditState=(state)=>{
-    this.setState({editState:state})
+  componentDidMount(){
+    this.$axios.get('/fm/admin/goods/getGoods')
+    .then((data)=>{
+      // console.log(data)
+      this.setState({list:data.data.list})
+      console.log(this.state.list)
+    })
+  }
+  changeEditState=(state,item)=>{
+    this.setState({editState:state,editData:item})
+    console.log(item)
   }
   changeComState=(state)=>{
     this.setState({comeState:state})
@@ -37,14 +35,14 @@ class GoodsList extends Component {
     this.setState({outState:state})
   }
   
-  render() { 
-    let {editState} = this.state
+  render() {
+    let {list,editState,comeState,outState,editData} = this.state
     return (
       <div>
         {/* 编辑商品 */}
-        {!editState || <GoodsEdit changeEditState={this.changeEditState}></GoodsEdit>}
-        {!this.state.comeState || <WareHousing　changeComState={this.changeComState}></WareHousing>}
-        {!this.state.outState ||　<OutHousing changeOutState={this.changeOutState}></OutHousing>}
+        {!editState || <GoodsEdit changeEditState={this.changeEditState} data={editData}></GoodsEdit>}
+        {!comeState || <WareHousing　changeComState={this.changeComState}></WareHousing>}
+        {!outState ||　<OutHousing changeOutState={this.changeOutState}></OutHousing>}
         {/* 商品列表 */}
         <div className="list-box">
           <div className="list-top">
@@ -53,29 +51,32 @@ class GoodsList extends Component {
             <hr/>
             <div className="list-find">
               <ul>
-                <li>
+                {/* <li>
                   <span>商品id: </span>
                   <InputNumber min={1} max={10} defaultValue={1} />
-                </li>
+                </li> */}
                 <li>
                   <span className="goodsname">商品名称：</span>
                   <Input placeholder="虾" />
                 </li>
                 <li>
                   <span>分类：</span>
-                  <select name="" className="goodscategory">
-                    <option value="">全部全部</option>
-                    <option value="">在售</option>
-                    <option value="">下架</option>
-                  </select>
+                  <Select defaultValue="lucy" style={{ width: 120 }}>
+                    <Option value="jack">Jack</Option>
+                    <Option value="lucy">Lucy</Option>
+                    <Option value="disabled" disabled>
+                      Disabled
+                    </Option>
+                    <Option value="Yiminghe">yiminghe</Option>
+                  </Select>
                 </li>
                 <li>
                   <span>商品状态：</span>
-                  <select name="" className="goodscategory">
-                    <option value="">全部</option>
-                    <option value="">在售</option>
-                    <option value="">下架</option>
-                  </select>
+                  <Select defaultValue="全部" style={{ width: 120 }}>
+                    <Option value="全部">全部</Option>
+                    <Option value="在售">在售</Option>
+                    <Option value="下架">下架</Option>
+                  </Select>
                 </li>
                 <li>
                   <Button type="primary">搜索</Button>
@@ -86,28 +87,28 @@ class GoodsList extends Component {
           </div>
           <List
             grid={{ gutter: 16, column: 3 }}
-            dataSource={data}
+            dataSource={list}
             renderItem={item => (
               <List.Item className="list-content">
-                <Card title={item.title}>
-                  <img src="https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1571720692&di=1935911ae38e195609ffe144b37f4ff6&src=http://sc.jb51.net/uploads/allimg/150504/14-150504120340102.jpg" alt=""/>
+                <Card title={item.name}>
+                  <img src={item.imgPath} alt=""/>
                   <ul>
                     <li>
-                      <p>商品id:55343</p>
-                      <p>分类:水果</p>
-                      <p>原价:12.22</p>
-                      <p>现价:10.05</p>
-                      <p>库存:555</p>
-                      <p>销量:555</p>
-                      <p>规格:/份</p>
-                      <p>原产地:福建</p>
+                      <p>商品id:{item.id}</p>
+                      <p>分类:{item.goodsType}</p>
+                      <p>原价:{item.oldPrice}</p>
+                      <p>现价:{item.nowPrice}</p>
+                      <p>库存:{item.counts}</p>
+                      <p>销量:{item.sale}</p>
+                      <p>规格:{item.guige}</p>
+                      <p>原产地:{item.addRess}</p>
                     </li>
                   </ul>
                   <div className="icons-list">
-                    <Icon type="edit" style={{width: "33%"}} onClick={this.changeEditState.bind(this,true)}/>
+                    <Icon type="edit" style={{width: "33%"}} onClick={this.changeEditState.bind(this,true,item)}/>
                     <Icon type="plus" style={{width: "33%"}} onClick={this.changeComState.bind(this,true)}/>
                     <Icon type="minus" style={{width: "33%"}} onClick={this.changeOutState.bind(this,true)}/>
-                  </div>
+                </div>
                 </Card>
               </List.Item>
             )}
